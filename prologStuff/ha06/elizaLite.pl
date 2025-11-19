c707e63cfc2b2abb71033682e3634baef049dffd
lmckenzie99 <157838863+lmckenzie99@users.noreply.github.com>
2025-11-19 10:41:07 -0500

    dump

--- prologStuff/ha06/elizaLite.pl
+++ prologStuff/ha06/elizaLite.pl
@@ -1,4 +1,3 @@
-% dictionary
 lookup(you, i).
 lookup(i, [why, do, you]).
 lookup(are, [am, not]).
@@ -6,13 +5,6 @@ lookup(do, ['no,']).
 lookup(portuguese, russian).
 lookup(A,A).
 
-% translate input to output
-% UI is user input
-% EO is eliza output
-% W is word at head of list
-% R is remaining tail of list
-% EW is eliza word(s)
-% ER is remaining eliza word(s)
 translate([], []).
 translate(UI, EO):-
     [W|R]=UI,
@@ -20,26 +12,17 @@ translate(UI, EO):-
     translate(R,ER),
     EO=[EW|ER].
 
-% output predicate
-% A is atom/list at head of list
-% L is remaining list
 output([]).
-
-% handling atoms only
 output([A|L]):-
   atom(A),
   write(A),
   write(' '),
   output(L).
-
-% if output has to smooth a list of words
-% example: lookup(i,[why,do,you])
 output([A|L]):-
   is_list(A),
   output(A),
   output(L).
 
-% interaction logic
 eliza :-
   nl,
   write('Ready > '),
